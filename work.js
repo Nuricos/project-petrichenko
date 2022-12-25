f93dd29af9ed7b298b45c892aea62059903ecc06
Nurlan <numraln.raiymbekov86@gmail.com>
2022-12-25 17:09:11 +0600

    homework2

--- work.js
+++ work.js
@@ -1,12 +1,40 @@
 "use strict";
 
-let num = 50;
-switch(num){
-    case 49:
-        console.log('ok');
-        break;
-    case 50:
-        console.log('ok2');
-        break;    
-}
+// let num = 50;
 
+// // while(num <= 55) {
+// //     console.log(num);
+// //     num++;
+// // }
+
+// do {
+//     console.log(num)
+//     num++;
+// } 
+
+// while(num < 55);
+
+
+// let result = '';
+
+// for(let i = 1; i < 7; i++) {
+
+//     for(let j = 0; j < i; j++) {
+//         result += '*';
+//     }
+//     result += '\n';
+   
+// }
+// console.log(result);
+
+
+first: for(let i = 0; i < 3; i++) {
+    console.log(`first level: ${i}`)
+    for(let j = 0; j < 3; j++) {
+        console.log(`second level ${j}`);
+        for(let k = 0; k < 3; k++) {
+            if(k === 2) continue first;
+            console.log(`third level ${k}`);
+        }
+    }   
+}
