9b95b92d1229b8a98e639b685907b9a7faa92669
Nurlan <numraln.raiymbekov86@gmail.com>
2022-12-25 16:25:31 +0600

    home work

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
+// const personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false
+// };
 
 
-let a = prompt("один из последних просмотренных фильмов?", ""),
-	b = prompt("на сколько вы его оцените?", ""),
-	c = prompt("один из последних просмотренных фильмов?", ""),
-	d = prompt("на сколько вы его оцените?", "");
+// let a = prompt("один из последних просмотренных фильмов?", ""),
+// 	b = prompt("на сколько вы его оцените?", ""),
+// 	c = prompt("один из последних просмотренных фильмов?", ""),
+// 	d = prompt("на сколько вы его оцените?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
---
+++ work.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let num = 50;
+switch(num){
+    case 49:
+        console.log('ok');
+        break;
+    case 50:
+        console.log('ok2');
+        break;    
+}
+
