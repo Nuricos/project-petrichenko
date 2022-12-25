6a10cbed03f6aead587c5740399af76ece917830
Nurlan <numraln.raiymbekov86@gmail.com>
2022-12-25 18:47:57 +0600

    new

--- work.js
+++ work.js
@@ -1,40 +1,21 @@
 "use strict";
 
-// let num = 50;
 
-// // while(num <= 55) {
-// //     console.log(num);
-// //     num++;
-// // }
-
-// do {
-//     console.log(num)
-//     num++;
-// } 
-
-// while(num < 55);
+let result = '';
 
 
-// let result = '';
+for(let i = 0; i < 6; i++) {
 
-// for(let i = 1; i < 7; i++) {
+    for(let k = 0; k < 6 - i; k++) {
+        result += ' ';
+    }
 
-//     for(let j = 0; j < i; j++) {
-//         result += '*';
-//     }
-//     result += '\n';
-   
-// }
-// console.log(result);
+    for(let j = 1; j < i * 2; j++) {
+       result += '*';
+    }
 
-
-first: for(let i = 0; i < 3; i++) {
-    console.log(`first level: ${i}`)
-    for(let j = 0; j < 3; j++) {
-        console.log(`second level ${j}`);
-        for(let k = 0; k < 3; k++) {
-            if(k === 2) continue first;
-            console.log(`third level ${k}`);
-        }
-    }   
+    
+    result += '\n';
 }
+
+console.log(result);
