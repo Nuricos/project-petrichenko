5128130cb982683d9830f8fc3c02eeebb136b53c
Nurlan <numraln.raiymbekov86@gmail.com>
2022-12-27 20:06:56 +0600

    nextup

--- script.js
+++ script.js
@@ -1,63 +1,80 @@
 "use strict";
 
-// let numberOfFilms;
 
-// function start() {
-//        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-//     while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-//     }
-// }
-// start();
+let personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
 
-// function rememberMyFilms() {
-//     for(let i = 0; i < 2; i++) {
-//         let a = prompt("один из последних просмотренных фильмов?", ""),
-//             b = prompt("на сколько вы его оцените?", "");
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+ 
+     while(personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+     }
+ },
+    rememberMyFilms: function() {
+    for(let i = 0; i < 2; i++) {
+        let a = prompt("один из последних просмотренных фильмов?", "").trim(),
+            b = prompt("на сколько вы его оцените?", "");
     
     
-//         if(a != null && b != null && a != '' && b != '' &&  a.length < 50) {
-//             personalMovieDB.movies[a] = b;
-//         } else {
-//             i--;
-//         }
-//     }
-// }
-// // rememberMyFilms();
+        if(a != null && b != null && a != '' && b != '' &&  a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
+    }
+    },
+    detectPersonalLevel: function() {
+        if(personalMovieDB.count < 10) {
+            console.log('просмотрено довольно мало фильмов');
+        } else if(personalMovieDB.count > 10 || personalMovieDB.count < 30) {
+            console.log('вы классический зритель');
+        } else if(personalMovieDB.count > 30) {
+            console.log('вы киноман');
+        }else {
+            console.log('произошла ошибка');
+        }
+    },
+    showMyDB: function(hidden) {
+        if(!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if(personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() { 
+        for(let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ваш любимый жанр по порядку ${i}`);
 
-// function detectPersonalLevel() {
-//     if(numberOfFilms < 10) {
-//         console.log('просмотрено довольно мало фильмов');
-//     } else if(numberOfFilms > 10 || numberOfFilms < 30) {
-//         console.log('вы классический зритель');
-//     } else if(numberOfFilms > 30) {
-//         console.log('вы киноман');
-//     }else {
-//         console.log('произошла ошибка');
-//     }
-// }
-// // detectPersonalLevel();
+            if(genre === '' || genre == null) {
+                console.log('Вы ввели некорректные данные');
+            } else {
+                personalMovieDB.genres[i-1] = genre;
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
+    }
+};
 
-// let personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-    
-// };
 
-// function showMyDB(hidden) {
-//     if(!hidden) {
-//         console.log(personalMovieDB);
-//     }
-// } 
-// showMyDB(personalMovieDB.privat);
 
-// function writeYourGenres() { 
-//     for(let i = 1; i <= 3; i++) {
-//         personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр по порядку ${i}`);
-//     }
-// }
-// writeYourGenres();
+
+
+
+
+
+
+
+
--- work.js
+++ work.js
@@ -1,27 +1,3 @@
 "use strict";
 
 
-function fibonacci(num) {
-    if (typeof(num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
-    return '';
-}
-        let str= '';
-    let one = 0;
-    let two = 1;
-    let three;
-    for(let i = 0; i < num; i++){
-        if(i + 1 === num) {
-            str += `${one}`;
-        } else {
-            str += `${one}`;
-        }
-        three = one + two;
-        one = two;
-        two = three;
-    }
-    return str;
-}
-console.log(fibonacci(1));
-
-
-
